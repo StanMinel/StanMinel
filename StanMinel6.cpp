a05acdd2805aa535eeae6d48619d624edfa31b89
StanMinel <35453296+StanMinel@users.noreply.github.com>
2019-04-03 09:45:24 +0300

    Add files via upload

---
+++ StanMinel6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    int M[100][100];
+    cout<<"n=";cin>>n;
+        {if(k!=i);
+        for(i=1;i<=n;i++);
+
+    }
+
+    {if(k!=j);
+        for(j=1;j<=n;j++);
+    }
+    {if(i!=j );
+        for(k=1;k<=n;k++);
+    }
+
+
+}
